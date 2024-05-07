215dc345deae3b0315fa92a6959ec1b3813736be
LinTan <116088937+LinTan-CSU@users.noreply.github.com>
2024-05-07 09:28:27 +0800

    Update Reverse Match.py

--- src/Reverse Match.py
+++ src/Reverse Match.py
@@ -5,30 +5,38 @@ import torch.utils.data as data
 import torch
 import csv
 
-test_dataset = Dataset('../data/reftest_pigment_52_airPLS1000_WS100_max_min.npy')
-test_dataloader = data.DataLoader(test_dataset, shuffle=False, batch_size=11)
-lib = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-csv_file = '../res/RM_pigment_52_airPLS1000_WS100_max_min.csv'
-outputs = []
+def RM(test_dataset, batch_size, lib):
+    test_dataset = Dataset(test_dataset)
+    test_dataloader = data.DataLoader(test_dataset, shuffle=False, batch_size=batch_size)
+    csv_file = '../res/RM.csv'
+    outputs = []
 
-with open(csv_file, 'w', newline='') as f:
-    writer = csv.writer(f)
+    with open(csv_file, 'w', newline='') as f:
+        writer = csv.writer(f)
 
-    for epoch in range(1):
+        for epoch in range(1):
 
-        for i, data in enumerate(test_dataloader, 0):
-            inputs = data
-            test = data[0, 1, :]
-            row_data = []
-            CCs = []
-            for j in range(11):
-                reference = data[j, 0, :]
-                peaks1, _ = find_peaks(reference, height=0.05)
-                correlation_coefficient = np.corrcoef(reference[peaks1], test[peaks1])[0, 1]
-                CCs.append(correlation_coefficient)
-            outputs = torch.tensor(CCs)
-            top_values, top_indices = torch.topk(outputs.view(-1), k=11)
+            for i, data in enumerate(test_dataloader, 0):
+                inputs = data
+                test = data[0, 1, :]
+                row_data = []
+                CCs = []
+                for j in range(batch_size):
+                    reference = data[j, 0, :]
+                    peaks1, _ = find_peaks(reference, height=0.05)
+                    correlation_coefficient = np.corrcoef(reference[peaks1], test[peaks1])[0, 1]
+                    CCs.append(correlation_coefficient)
+                outputs = torch.tensor(CCs)
+                top_values, top_indices = torch.topk(outputs.view(-1), k=batch_size)
           
-            for k in range(11):
-                row_data.extend([lib[top_indices[k]], float(top_values[k])])
-            writer.writerow(row_data)
+                for k in range(batch_size):
+                    row_data.extend([lib[top_indices[k]], float(top_values[k])])
+                writer.writerow(row_data)
+    return row_data
+
+if __name__ == '__main__':
+    test_dataset = '../data/test_dataset.npy'
+    batch_size = 10
+    lib = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+    RM(test_dataset, batch_size, lib)
+    
