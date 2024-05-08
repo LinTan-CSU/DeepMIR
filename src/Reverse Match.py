e5fb71638d894df5886c4f4818b8a015fd791e41
LinTan <116088937+LinTan-CSU@users.noreply.github.com>
2024-05-08 11:49:50 +0800

    Update Reverse Match.py

--- src/Reverse Match.py
+++ src/Reverse Match.py
@@ -1,15 +1,27 @@
 import numpy as np
 from scipy.signal import find_peaks
 from Dataset import Dataset
-import torch.utils.data as data
+import torch.utils.data
 import torch
 import csv
+import pandas as pd
 
-def RM(test_dataset, batch_size, lib):
-    test_dataset = Dataset(test_dataset)
-    test_dataloader = data.DataLoader(test_dataset, shuffle=False, batch_size=batch_size)
+def RM(test_dataset_path, lib):
+    """
+    * RM
+    *
+    * Attributes
+    * ----------
+    * test_dataset_path : File path for storing the test dataset
+    * lib : List of names of reference components
+    *
+    * Returns
+    * -------
+    * results : the predicted results
+    """
+    test_dataset = Dataset(test_dataset_path)
+    test_dataloader = torch.utils.data.DataLoader(test_dataset, shuffle=False, batch_size=len(lib))
     csv_file = '../res/RM.csv'
-    outputs = []
 
     with open(csv_file, 'w', newline='') as f:
         writer = csv.writer(f)
@@ -17,25 +29,29 @@ def RM(test_dataset, batch_size, lib):
         for epoch in range(1):
 
             for i, data in enumerate(test_dataloader, 0):
-                inputs = data
                 test = data[0, 1, :]
                 row_data = []
                 CCs = []
-                for j in range(batch_size):
+                for j in range(len(lib)):
                     reference = data[j, 0, :]
                     peaks1, _ = find_peaks(reference, height=0.05)
                     correlation_coefficient = np.corrcoef(reference[peaks1], test[peaks1])[0, 1]
                     CCs.append(correlation_coefficient)
                 outputs = torch.tensor(CCs)
-                top_values, top_indices = torch.topk(outputs.view(-1), k=batch_size)
-          
-                for k in range(batch_size):
+                top_values, top_indices = torch.topk(outputs.view(-1), k=len(lib))
+
+                for k in range(len(lib)):
                     row_data.extend([lib[top_indices[k]], float(top_values[k])])
                 writer.writerow(row_data)
-    return row_data
+    results = pd.read_csv(csv_file, header=None)
+
+    return results
+
 
 if __name__ == '__main__':
     test_dataset = '../data/Quinary.npy'
-    batch_size = 12
-    lib = ['1,2-dichloroethane', '1-butanol', 'acetonitrile', 'cyclohexane', 'dichloromethane', 'diethylene_glycol_dimethyl_ether', 'ethanol', 'hexane', 'isopropyl_alcohol', 'methanol', 'toluene', 'trichloromethane']
-    RM(test_dataset, batch_size, lib)
+    lib = ['1,2-dichloroethane', '1-butanol', 'acetonitrile', 'cyclohexane', 'dichloromethane',
+           'diethylene_glycol_dimethyl_ether', 'ethanol', 'hexane', 'isopropyl_alcohol', 'methanol', 'toluene',
+           'trichloromethane']
+    results = RM(test_dataset, lib)
+    print(results)
